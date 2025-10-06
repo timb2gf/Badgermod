93a7886df2835e462cc95c08a05a3a8c675fae5a
timb2gf <81277309+timb2gf@users.noreply.github.com>
2025-10-06 13:48:52 -0400

    added m4 files

--- Resources/scripts/missions/level01/m4sdi.lua
+++ Resources/scripts/missions/level01/m4sdi.lua
@@ -1,31 +1,38 @@
-Game.SelectMission("m2sd");
+Game.SelectMission("m4sd");
+
+Game.SetMissionStartCameraName( "mission4camShape" );
+Game.SetMissionStartMulticontName( "mission4cam" );
+Game.SetAnimatedCameraName( "mission4camShape" );
+Game.SetAnimCamMulticontName( "mission4cam" );
 
 Game.SetMissionResetPlayerOutCar("m4_homer_start", "m4_carstart");
 Game.SetDynaLoadData("l1r4a.p3d;l1z6.p3d;l1r6.p3d;");
 
-Game.UsePedGroup( 0 ); 
+Game.UsePedGroup(7); 
 
 Game.AddStage(9);
 	Game.SetMaxTraffic(2);
 	Game.SetStageMessageIndex(08);
 	Game.SetHUDIcon( "pwrplant" );
+	Game.SetPresentationBitmap( "art/frontend/dynaload/images/mis01_04.p3d" );
 	Game.AddObjective("goto","both");
 		Game.SetDestination("m4_powerplant", "carsphere");
-		Game.AddNPC("burns", "m4_carl_sd");
 		Game.SetCollectibleEffect("wrench_collect");
 	Game.CloseObjective();
 Game.CloseStage();
 
 Game.AddStage();
-	Game.SetStageMessageIndex(254);
+	Game.RESET_TO_HERE();
+	Game.SetStageMessageIndex(154);
 	Game.SetHUDIcon( "wstation" );
 	Game.AddObjective("goto");
 		Game.TurnGotoDialogOff();
+		Game.AddNPC("carl", "m4_carl_sd");
 		Game.SetDestination("m4_locator_sd", "triggersphere");
 		Game.SetCollectibleEffect("wrench_collect");
 		Game.MustActionTrigger();
 	Game.CloseObjective();
-	--Game.SetCompletionDialog("camera");
+	--SetCompletionDialog("camera");
 Game.CloseStage();
 
 Game.AddStage(0);
@@ -33,21 +40,18 @@ Game.AddStage(0);
 		Game.AmbientAnimationRandomize( 1, 0 );      -- ( pc=0, npc=1) (nonrandom=0, random=1)
 		Game.AmbientAnimationRandomize( 0, 0 );
 		Game.AddAmbientNpcAnimation( "none" );
-		Game.AddAmbientPcAnimation( "none" );
-		Game.SetDialogueInfo("homer","burns","sleep",0);
-		Game.SetFadeOut( 2 );
+		Game.AddAmbientPcAnimation( "dialogue_hands_in_air" );
+		Game.SetDialogueInfo("homer","carl","camera",0);
 	Game.CloseObjective();
 Game.CloseStage();
 
-Game.AddStage();
-	Game.AddObjective("timer");
-		Game.SetDurationTime(3);
-		Game.SetStageCharacterModel("burns");
-		Game.TurnGotoDialogOff();
-		Game.SetPresentationBitmap( "art/frontend/dynaload/images/mis01_02.p3d" );
-		Game.AddStageCharacter ("homer", "m1_smithers", "", "", "");
-		Game.AddStageVehicle("burns_v","m1_smitherscar","target","Missions\\level01\\M1race.con");
-	Game.CloseObjective();
-Game.CloseStage();
+--AddStage(0);
+--	SetStageMessageIndex(252);
+--	SetHUDIcon( "pwrplant" );
+--	AddObjective( "goto" );
+--		SetDestination( "m4_start_sd", "triggersphere");
+--		SetCollectibleEffect("wrench_collect");
+--	CloseObjective();
+--CloseStage();
 
 Game.CloseMission();
---
+++ Resources/scripts/missions/level01/m4sdl.lua
@@ -0,0 +1,4 @@
+Game.LoadP3DFile("art\\missions\\level01\\mission4cam.p3d");
+
+Game.LoadP3DFile( "art\\frontend\\dynaload\\images\\msnicons\\location\\pwrplant.p3d" );
+Game.LoadP3DFile( "art\\frontend\\dynaload\\images\\msnicons\\location\\wstation.p3d" );
