58cfbc2f6aecfb78f2fa858910065aefd49b5918
timb2gf <81277309+timb2gf@users.noreply.github.com>
2025-10-22 01:28:45 -0400

    m6 start

---
+++ Resources/scripts/missions/level01/m6sdi.lua
@@ -0,0 +1,64 @@
+Game.SelectMission("m6sd");
+
+Game.SetMissionResetPlayerOutCar("m4_homer_start", "m4_carstart");
+ame.SetDynaLoadData("l1r4a.p3d;l1z6.p3d;l1r6.p3d;");
+
+Game.UsePedGroup(3); 
+
+Game.AddStage(0);
+	Game.SetStageMessageIndex(12);
+	Game.AddObjective("getin", "neither");
+		Game.SetObjTargetVehicle("current");
+	Game.CloseObjective();
+Game.CloseStage();
+
+Game.AddStage(5);
+	Game.RESET_TO_HERE();
+	Game.SetStageMessageIndex(00);
+	Game.SetHUDIcon("marage");
+	Game.AddObjective("talkto");
+		Game.AddNPC("marge", "m6_marge_sd");
+		Game.AddObjectiveNPCWaypoint( "marge", "m6_marge_walk_2" );
+		Game.AddObjectiveNPCWaypoint( "marge", "m6_marge_walk_1" );
+		Game.SetTalkToTarget("marge", 0, 0.2);
+	Game.CloseObjective();
+Game.CloseStage();
+
+Game.AddStage(0);
+	Game.AddObjective("dialogue");
+		Game.SetPresentationBitmap( "art/frontend/dynaload/images/mis01_06.p3d" );
+		Game.AmbientAnimationRandomize( 1, 0 );      -- ( pc=0, npc=1) (nonrandom=0, random=1)
+		Game.AmbientAnimationRandomize( 0, 0 );
+		Game.SetConversationCam( 0, "pc_near" );
+		Game.SetConversationCam( 1, "npc_far" );
+		Game.SetConversationCam( 2, "pc_near" );
+		Game.SetConversationCam( 3, "npc_near" );
+		Game.SetConversationCam( 4, "pc_far" );
+		Game.AddAmbientNpcAnimation( "none" );
+		Game.AddAmbientNpcAnimation( "dialogue_cross_arms" );
+		Game.AddAmbientNpcAnimation( "none" );
+		Game.AddAmbientNpcAnimation( "dialogue_shaking_fist" );
+		Game.AddAmbientNpcAnimation( "none" );
+		Game.AddAmbientPcAnimation( "dialogue_hands_in_air" );
+		Game.AddAmbientPcAnimation( "none" );
+		Game.AddAmbientPcAnimation( "dialogue_yes" );
+		Game.AddAmbientPcAnimation( "none" );
+		Game.AddAmbientPcAnimation( "dialogue_scratch_head" );
+		Game.SetCamBestSide( "m6_end");
+		Game.SetDialogueInfo("homer","marge","violent",0);
+		Game.SetDialoguePositions("m6_homer","m6_homer_start","m6_marge_walk_2");
+	Game.CloseObjective();
+Game.CloseStage();
+
+Game.CloseMission();
+
+--dialogue_hands_in_air
+--dialogue_hands_on_hips
+--dialogue_scratch_head
+--dialogue_shaking_fist
+--dialogue_thinking
+--dialogue_yes
+--dialogue_no
+--dialogue_cross_arms
+--dialogue_open_arm_hand_gesture
+--dialogue_shake_hand_in_air
